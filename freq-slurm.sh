d7fcd2631c47eaf83bd8a6088389dde3815dd3ef
AWS ParallelCluster user <ec2-user@ip-10-0-6-67.ap-southeast-1.compute.internal>
2025-09-11 17:13:00 +0000

    Add own scripts for Lab 6A

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -14,8 +14,6 @@
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
-import time; \
 pyh.FreqSpectrum(saveLevel=1); \
-pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1);
-print(time.localtime());"
+pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1);"
 
